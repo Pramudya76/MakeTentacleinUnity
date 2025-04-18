7125c73f78d21bc5850e2d20f4758348bce88c4d
Pramudya76 <pramudyarama76@gmail.com>
2025-04-18 16:20:20 +0700

    Tentacle (75%)

--- Assets/Script/TentacleMove.cs
+++ Assets/Script/TentacleMove.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class TentacleMove : MonoBehaviour
 {
+    private float startY;
+    private float moveTentacle = 6f;
+    private float moveSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveY = startY + Mathf.PingPong(Time.time * moveSpeed, moveTentacle);
+        transform.position = new Vector2(transform.position.x, moveY);
     }
 }
