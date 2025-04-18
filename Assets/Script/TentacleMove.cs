756af6fd535b4c89512c8572fddf2a8bc9553d52
Pramudya76 <pramudyarama76@gmail.com>
2025-04-19 02:03:58 +0700

    Membuat Tentacle (100% -> meskipun ada yg kurang puas pd mekanismenya)

--- Assets/Script/TentacleMove.cs
+++ Assets/Script/TentacleMove.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class TentacleMove : MonoBehaviour
 {
     private float startY;
-    private float moveTentacle = 6f;
-    private float moveSpeed = 5f;
+    private float startX;
+    public float moveTentacle = 3f;
+    private float moveSpeed = 1f;
+    public float waktuJeda;
     // Start is called before the first frame update
     void Start()
     {
         startY = transform.position.y;
+        startX = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveY = startY + Mathf.PingPong(Time.time * moveSpeed, moveTentacle);
-        transform.position = new Vector2(transform.position.x, moveY);
+        float moveY = startY + Mathf.Sin((Time.time + waktuJeda) * moveSpeed) * moveTentacle;
+        float moveX = startX + Mathf.Sin((Time.time + waktuJeda) * moveSpeed) * moveTentacle;
+        transform.position = new Vector2(moveX, moveY);
     }
+
+    
+
 }
